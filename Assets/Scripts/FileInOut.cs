ead478bdda46edc5db89097243f0129330160e40
skdjnfbfhf <yeongseohwang317@gmail.com>
2025-08-12 21:30:08 +0900

    kuk

---
+++ Assets/Scripts/FileInOut.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Experimental.AI;
+using UnityEngine.Rendering;
+
+public class PlayerData
+{
+    public string Name;
+    public int Stage;
+    public int Score;
+}
+public class FileInOut : MonoBehaviour
+{
+    public static FileInOut instance = null;
+    //파일 저장할 이름
+    public string textFileName = "textPlayer.txt";
+    public string jsonFileName = "jsonPlayer.json";
+    //정보를 저장할 폴더
+    public string folderName = "PlayerData";
+
+    string folderPath;
+    string txtPath;
+    string jsonPath;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        //다양한 플랫폼에서 ㅅ ㅏ ㅇ ㅛ ㅇ
+        Debug.Log(Application.persistentDataPath);
+        //횬쟈 여플리케이션 경로
+        Debug.Log(Application.dataPath);
+        folderPath = Path.Combine(Application.dataPath, folderName);
+        txtPath = Path.Combine(Application.dataPath, folderName, textFileName);
+        jsonPath = Path.Combine(Application.dataPath, folderName, jsonFileName);
+    }
+
+    #region txt 파일
+    public void CreateFolder()
+    {
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+            Debug.Log("폴더 생성 완료");
+        }
+    }
+
+
+    public void SaveTxt(string content)
+    {
+        CreateFolder();
+        File.WriteAllText(txtPath, content);
+
+        Debug.Log("txt 파일 저장 완료");
+    }
+
+    public string LoadTxt()
+    {
+        if (File.Exists(txtPath))
+        {
+            return File.ReadAllText(txtPath);
+        }
+        Debug.Log("txt 파일 로드 실패");
+        return null;
+    }
+
+    public void UpdateFile(string newContent)
+    {
+        if (File.Exists(txtPath))
+        {
+            File.WriteAllText(txtPath, newContent);
+            Debug.Log("txt 파일 수정 완료");
+        }
+        else
+        {
+            Debug.LogWarning("txt 파일 수정 실패");
+        }
+    }
+
+    public void DeleteFile()
+    {
+        if (File.Exists(txtPath))
+        {
+            File.Delete(txtPath);
+            Debug.Log("txt 파일 삭제 완료");
+        }
+        else
+        {
+            Debug.LogWarning("txt 파일 삭제 실패");
+        }
+    }
+    #endregion
+
+#region json 파일
+
+    public void SaveJson(PlayerData player)
+    {
+
+        string jsonString = JsonUtility.ToJson(player, true);
+        File.WriteAllText(jsonPath, jsonString);
+        Debug.Log("JSON 저장 완료");
+    }
+
+    public PlayerData LoadJson()
+    {
+        if(File.Exists(jsonPath))
+        {
+            string json = File.ReadAllText(jsonPath);
+            return JsonUtility.FromJson<PlayerData>(json);
+        }
+        else
+        {
+            Debug.LogWarning("json 파일 로드 실패");
+            return null;
+        }
+    }
+
+    public void UpdateJsonField(string name, int stage, int score)
+    {
+        PlayerData data = LoadJson();
+        if(data != null)
+        {
+            data.Name = name;
+            data.Stage = stage;
+            data.Score = score;
+            SaveJson(data);
+            Debug.Log("json 파일 저장 완료");
+        }
+    }
+
+    public void DeleteJson()
+    {
+        if(File.Exists(jsonPath))
+        {
+            File.Delete(jsonPath);
+            Debug.Log("json 파일 삭제 완료");
+        }
+    }
+    #endregion
+
+
+
+}
---
+++ Assets/Scripts/FileManager.cs
@@ -0,0 +1,118 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileManager : MonoBehaviour
+{
+    public InputField textInput;
+    public InputField nameInput;
+    public InputField stageInput;
+    public InputField scoreInput;
+    public Text outputText;
+
+
+    public void OnSaveText()
+    {
+        if (!string.IsNullOrEmpty(textInput.text))
+        {
+            FileInOut.instance.SaveTxt(textInput.text);
+            outputText.text = "txt 저장 완료";
+        }
+        else
+        {
+            outputText.text = "txt 저장 실패";
+        }
+    }
+
+    public void OnLoadText()
+    {
+        string result = FileInOut.instance.LoadTxt();
+
+        if (result != null)
+        {
+            outputText.text = result;
+        }
+        else
+        {
+            outputText.text = "text 파일 로드 실패";
+        }
+
+    }
+
+    public void OnAppendText()
+    {
+        if (!string.IsNullOrEmpty(textInput.text))
+        {
+            FileInOut.instance.UpdateFile(textInput.text);
+            outputText.text = "txt 수정 완료";
+        }
+        else
+        {
+            outputText.text = "txt 수정 실패";
+        }
+    }
+
+    public void OnDeleteText()
+    {
+        FileInOut.instance.DeleteFile();
+        outputText.text = "TXT 삭제 완료!";
+    }
+
+    public void OnSaveJson()
+    {
+        try
+        {
+
+            PlayerData player = new PlayerData();
+            player.Name = nameInput.text;
+            player.Stage = int.Parse(stageInput.text);
+            player.Score = int.Parse(stageInput.text);
+
+            FileInOut.instance.SaveJson(player);
+            outputText.text = "json 저장 성공";
+        }
+        catch (System.Exception e)
+        {
+            outputText.text = "JSon 저장 실패";
+        }
+    }
+
+
+    public void OnLoadJson()
+    {
+        PlayerData player = FileInOut.instance.LoadJson();
+        if (player != null)
+        {
+            outputText.text = "이름: " + player.Name + ", 레벨: " + player.Stage + ", 점수: " + player.Score;
+        }
+        else
+        {
+            outputText.text = "json 로드 실패";
+        }
+    }
+
+    public void OnUpdateJson()
+    {
+        try
+        {
+            FileInOut.instance.UpdateJsonField(nameInput.text, int.Parse(stageInput.text), int.Parse(scoreInput.text));
+            outputText.text = "JSON 수정 완료!";
+        }
+        catch(System.Exception e)
+        {
+            outputText.text = $"JSON 수정 실패: {e.Message}";
+        }
+    }
+
+    public void OnDeleteJson()
+    {
+        FileInOut.instance.DeleteJson();
+        outputText.text = "JSON 삭제 완료!";
+    }
+    
+}
